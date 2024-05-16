6420f82968a1f6a2b7780083a363eb2906ac1db4
AlanCasGar <alan.castanedadycmc.com.mx>
2024-05-16 17:04:05 -0600

    Conexion DB

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DatabaseService } from '../services/database.service';
 
 @Component({
   selector: 'app-tab2',
@@ -7,13 +8,35 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
+  DB: any = [];
+  List: any = [];
+  
   SHOW = false;
   EDIT = true;
+  
+  Filter: string = "";
+  
+  constructor(private database:DatabaseService) {}
 
-  constructor() {}
+  ngOnInit(){
+    this.database.get().subscribe(data => {
+      this.DB = data
+    })
+  }
 
   show(){
     this.SHOW = true
   }
 
+  filt(){
+    var filter = this.Filter
+
+    this.List = this.DB.filter(function (el:any){
+      if(filter == el.Corte){
+        return el.Corte.toString().toLowerCase().includes(
+          filter.toString().trim().toLowerCase
+        )
+      }
+    })
+  }
 }
